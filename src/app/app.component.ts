6bc979603d0f519000f79e425e6c730d6815ea6b
tumit <tumit@live.com>
2018-10-17 13:30:23 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng6-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng6-demo';
+}
